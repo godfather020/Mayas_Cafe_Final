e7c463f8eb3d1ac338f1ae353ba6caaf2814bcdc
Savan <sajwanrah020@gmail.com>
2022-04-19 14:26:18 +0530

    Commit

--- app/src/main/java/com/example/mayasfood/activity/GetStart.java
+++ app/src/main/java/com/example/mayasfood/activity/GetStart.java
@@ -1,26 +1,41 @@
 package com.example.mayasfood.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.mayasfood.R;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
 public class GetStart extends AppCompatActivity {
 
     private boolean isBackPressed = false;
+    ArrayList<String> permission = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_start);
         Button getStart = findViewById(R.id.get_start);
+
+        if (check_Permission()) {
+            Log.d("permission", "granted");
+        } else {
+            requestPermission();
+        }
 
         getStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,5 +65,56 @@ public class GetStart extends AppCompatActivity {
                 isBackPressed = false;
             }
         },2000);
+    }
+
+    boolean check_Permission() {
+        int camera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int internet = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        int gallery =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int gallery1 =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int contact = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        int phone = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+        int recieveSms = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        if (camera != PackageManager.PERMISSION_GRANTED) {
+            permission.add(Manifest.permission.CAMERA);
+        }
+        if (internet != PackageManager.PERMISSION_GRANTED) {
+            permission.add(Manifest.permission.INTERNET);
+        }
+        if (gallery != PackageManager.PERMISSION_GRANTED) {
+            permission.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        if (gallery1 != PackageManager.PERMISSION_GRANTED) {
+            permission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (contact != PackageManager.PERMISSION_GRANTED) {
+            permission.add(Manifest.permission.READ_CONTACTS);
+        }
+        if (phone != PackageManager.PERMISSION_GRANTED) {
+            permission.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (recieveSms != PackageManager.PERMISSION_GRANTED){
+            permission.add(Manifest.permission.RECEIVE_SMS);
+        }
+        return permission.isEmpty();
+
+
+
+    }
+
+    void requestPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                permission.toArray(new String[0]),
+                Companion.REQUEST_ID_MULTIPLE_PERMISSIONS
+
+        );
+    }
+
+    private static class Companion {
+
+        static int REQUEST_ID_MULTIPLE_PERMISSIONS = 3;
     }
 }
